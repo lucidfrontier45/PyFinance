28225aebced41629cb37c3cd9d40903c09c549ab
Shiqiao Du <lucidfrontier.45@gmail.com>
2013-12-10 01:38:20 +0900

    fixed bug

--- pyfinance/strategy/stoch_cross.py
+++ pyfinance/strategy/stoch_cross.py
@@ -12,7 +12,7 @@ def findSTOCHCross(ts, lowerbound=30, fastk_period=14, slowk_period=3, slowd_per
     STOCH = talib.abstract.STOCH
     data = ts.toDict()
     slowk, slowd = STOCH(data, fastk_period=fastk_period, slowk_period=slowk_period, slowd_period=slowd_period)
-    mask1 = np.where(np.diff(np.sign(slowd - slowk))==2)[0]
+    mask1 = np.where(np.diff(np.sign(slowk - slowd))==2)[0]
     mask2 = np.where(slowd < lowerbound)[0]
     mask = list(set(mask1).intersection(set(mask2)))
     mask.sort()
@@ -25,14 +25,14 @@ class STOCHCrossOptimizeStrategy(_BaseOptimizeStrategy):
         
     def _fit(self, ts, roc):
         max_score = -1.0
-#         for lowerbound in xrange(20, 30):
-        lowerbound=30
-        for fastk_period in xrange(9, 20):
-            for slowk_period in xrange(3, 7):
-                for slowd_period in xrange(3, 7):
-                    params = (lowerbound, fastk_period, slowk_period, slowd_period)
-                    score = self.score(ts, roc, params=params)
-                    if score > max_score:
-                        max_score = score
-                        max_params = params
+        for lowerbound in xrange(25, 26):
+#         lowerbound=30
+            for fastk_period in xrange(9, 20):
+                for slowk_period in xrange(3, 7):
+                    for slowd_period in xrange(3, 7):
+                        params = (lowerbound, fastk_period, slowk_period, slowd_period)
+                        score = self.score(ts, roc, params=params)
+                        if score > max_score:
+                            max_score = score
+                            max_params = params
         return max_score, max_params
